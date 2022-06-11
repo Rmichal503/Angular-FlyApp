cf026b5d9493d40c4a6acaf43af6a9936110285b
Rmichal503 <rakmichal503@gmail.com>
2022-06-11 09:51:47 +0200

    ngIfToken

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, DoCheck} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from '../login/login.component';
+import { LoginService } from 'src/app/services/login.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements DoCheck {
 
-  constructor(private loginModal: NgbModal) { }
+  constructor(private loginModal: NgbModal, private mongo: LoginService) { }
   login() {
     this.loginModal.open(LoginComponent, { size: 'sm' });
   }
-  ngOnInit(): void {
+  token:string;
+  ngDoCheck(): void {
+    this.token = this.mongo.token
   }
 
 }
