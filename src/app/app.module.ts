68941be822edc62f9d22135fd0696f85dddb400d
Rmichal503 <rakmichal503@gmail.com>
2022-06-20 20:40:41 +0200

    logout and locale_id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl'
+registerLocaleData(localePl)
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,7 +46,7 @@ import { TypeOfSeatsPipe} from './pipes/type-of-seats.pipe';
   //   MymodalComponent
   // ],
   providers: [FlyApiService,
-    NgbActiveModal],
+    NgbActiveModal, {provide: LOCALE_ID, useValue: 'pl-PL'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
